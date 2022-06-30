2adc4bf95fd158cfb9ba8d249c0448959fad6309
SmartRenaldo <correctgavin@gmail.com>
2022-07-01 01:22:03 +0930

    assignment 1

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
+  // styleUrls: ['./warning-alert.component.css']
   styles: [`
     h3 {
       color: blue;
